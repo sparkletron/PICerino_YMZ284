9f2a6917730c370ef69473d044a9a1db97857d40
Jay Convertino <electrobs@gmail.com>
2022-01-28 09:58:40 -0500

    added code to test all channels in all various modes, seperate at this point.

--- src/lib_picSN76489/test/soundTest.c
+++ src/lib_picSN76489/test/soundTest.c
@@ -37,45 +37,194 @@ uint8_t g_porteBuffer = 0;
 
 void main(void) 
 {
-    struct s_sn76489 sn76489;
+  uint16_t freq = 0;
+  uint8_t attn = 0;
+  uint8_t shiftRate = 0;
+  
+  struct s_sn76489 sn76489;
+
+  /* OSCCON SETUP */
+  OSCCONbits.IRCF = 0x7;
+  OSCCONbits.OSTS = 0;
+  OSCCONbits.SCS  = 0x3;
+
+  OSCCON2bits.PLLEN = 1;
+
+  /* PORT E SETUP */
+  INTCON2bits.nRBPU = 1;
+
+  /* disable analog inputs */
+  ANSELA = 0;
+  ANSELE = 0;
+  ANSELD = 0;
+
+  /* Port E set all to output */
+  TRISE = 0;
+
+  LATE = 0;
+
+  g_porteBuffer = 1;
+
+  /* wait for chip to be ready */
+  __delay_ms(10);
+
+  initSN76489port(&sn76489, &TRISA, &TRISD, 6, 7, 5);
+
+  initSN76489(&sn76489, &LATA, &PORTD, &LATD);
+
+  /* chip tests */
+  /* voice one freq test */
+  LATE = (1 << 0);
+  
+  setSN76489voice1_attn(&sn76489, 2);
+  
+  do
+  {
+    setSN76489voice1_freq(&sn76489, freq);
     
-    /* OSCCON SETUP */
-    OSCCONbits.IRCF = 0x7;
-    OSCCONbits.OSTS = 0;
-    OSCCONbits.SCS  = 0x3;
+    __delay_ms(5);
     
-    OSCCON2bits.PLLEN = 1;
+    freq++;
     
-    /* PORT E SETUP */
-    INTCON2bits.nRBPU = 1;
+  } while(freq < (1 << 10));
+  
+  setSN76489voice1_freq(&sn76489, 254);
+  
+  /* voice one attn test */
+  do
+  {
+    setSN76489voice1_attn(&sn76489, attn);
     
-    /* disable analog inputs */
-    ANSELA = 0;
-    ANSELE = 0;
-    ANSELD = 0;
+    __delay_ms(500);
     
-    /* Port E set all to output */
-    TRISE = 0;
+    attn++;
     
-    LATE = 0;
+  } while(attn < (1 << 4));
+
+  /* voice two freq test */
+  freq = 0;
+  attn = 0;
+  
+  LATE = (1 << 1);
+  
+  setSN76489voice2_attn(&sn76489, 2);
+  
+  do
+  {
+    setSN76489voice2_freq(&sn76489, freq);
     
-    g_porteBuffer = 1;
+    __delay_ms(5);
     
-    //wait for chip to be ready
-    __delay_ms(10);
+    freq++;
     
-    initSN76489port(&sn76489, &TRISA, &TRISD, 6, 7, 5);
+  } while(freq < (1 << 10));
+  
+  setSN76489voice2_freq(&sn76489, 254);
+  
+  /* voice two attn test */
+  do
+  {
+    setSN76489voice2_attn(&sn76489, attn);
     
-    initSN76489(&sn76489, &LATA, &PORTD, &LATD);
+    __delay_ms(500);
     
-//     setSN76489voice1_freq(&sn76489, 112);
+    attn++;
     
-//     setSN76489voice1_attn(&sn76489, 8);
+  } while(attn < (1 << 4));
+  
+  /* voice three freq test */
+  freq = 0;
+  attn = 0;
+  
+  LATE = (1 << 2);
+  
+  setSN76489voice3_attn(&sn76489, 2);
+  
+  do
+  {
+    setSN76489voice3_freq(&sn76489, freq);
     
-    for(;;)
-    {
-      LATE = g_porteBuffer;
-      g_porteBuffer = (g_porteBuffer == 4 ? 1 : g_porteBuffer << 1);
-      __delay_ms(1000);
-    }
+    __delay_ms(5);
+    
+    freq++;
+    
+  } while(freq < (1 << 10));
+  
+  setSN76489voice3_freq(&sn76489, 254);
+  
+  /* voice three attn test */
+  do
+  {
+    setSN76489voice3_attn(&sn76489, attn);
+    
+    __delay_ms(500);
+    
+    attn++;
+    
+  } while(attn < (1 << 4));
+  
+  /* test noise */
+  attn = 0;
+  
+  setSN76489noise_attn(&sn76489, 2);
+  
+  /** Periodic noise **/
+  do
+  {
+    setSN76489noiseCtrl(&sn76489, 0, shiftRate);
+    
+    __delay_ms(2000);
+    
+    shiftRate++;
+    
+  } while(shiftRate < (1 << 2));
+  
+  /* noise attn test */
+  do
+  {
+    setSN76489noise_attn(&sn76489, attn);
+    
+    __delay_ms(500);
+    
+    attn++;
+    
+  } while(attn < (1 << 4));
+  
+  shiftRate = 0;
+  
+  setSN76489noise_attn(&sn76489, 2);
+  
+  /** White noise **/
+  do
+  {
+    setSN76489noiseCtrl(&sn76489, 1, shiftRate);
+    
+    __delay_ms(2000);
+    
+    shiftRate++;
+    
+  } while(shiftRate < (1 << 2));
+  
+  attn = 0;
+  
+  /* noise attn test */
+  do
+  {
+    setSN76489noise_attn(&sn76489, attn);
+    
+    __delay_ms(500);
+    
+    attn++;
+    
+  } while(attn < (1 << 4));
+  
+  /* done testing */
+  LATE = 0;
+  
+  for(;;)
+  {
+    LATE = g_porteBuffer;
+    g_porteBuffer = (g_porteBuffer == 4 ? 1 : g_porteBuffer << 1);
+    __delay_ms(100);
+  }
 }
